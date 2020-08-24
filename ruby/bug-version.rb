84acb050bbca7d4a45f87327832788c59da1370a
hzhang934748656 <44934005+hzhang934748656@users.noreply.github.com>
2020-08-24 11:27:28 -0400

    Update bug-version.rb

--- ruby/bug-version.rb
+++ ruby/bug-version.rb
@@ -1,33 +1,38 @@
-
 require 'set'
 =begin
 Input Pattern : 
-0 0 0 0 0 
-0 0 1 0 0
-0 0 1 0 0
-0 0 1 0 0 
-0 0 0 0 0
+
+00000000000
+00000000000
+00011000000
+00010100000
+00010000000
+00000000000
+00000000000
+00000000000
+00000000000
+00000000000
+00000000000
+
 Output Pattern :
-If the generation number is odd : | If the generation number is even :
-								  |
-0 0 0 0 0                         | 0 0 0 0 0
-0 0 0 0 0                         | 0 0 1 0 0
-0 1 1 1 0                         | 0 0 1 0 0
-0 0 0 0 0                         | 0 0 1 0 0
-0 0 0 0 0                         | 0 0 0 0 0
-								  |
+
+00000000000
+00000000000
+00011000000
+00011000000
+00000000000
+00000000000
+00000000000
+00000000000
+00000000000
+00000000000
+00000000000
 =end
 def display(world,i)
     puts "Generation #{i+1}:"
-    x_list = []
-    y_list = []
-    world.each do |item|
-       x_list.push(item[0])
-       y_list.push(item[1])
-    end
     ###############print the map###################
-    for y in (y_list.min-3..y_list.max+3) do
-        for x in (x_list.min-3..x_list.max+3) do
+    for y in (0..10) do
+        for x in (0..10) do
             if world.include?([x,y])
                 print "1"
             else
@@ -38,8 +43,6 @@ def display(world,i)
     end
 end 
 
-
-neighbor = [[-1,1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
           
 def location(cell,center)
     #################get the pattern and add it location############# 
@@ -69,6 +72,7 @@ def generation(world,n,neighbor)
         ##############update map######################
         world = new_world
     end
+    return world
 end
 
 #world = Set[[1,1],[2,2],[2,1],[1,2]]
@@ -77,6 +81,15 @@ end
 ############main program###############
 #######################################
 glider = Set[[0,1],[1,0],[0,0],[0,2],[2,1]]
+neighbor = [[-1,1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
 world = location(glider,[3,2])
-generation(world,10,neighbor)
-
+user_world = generation(world,5,neighbor)
+correct_answer = [[3, 3], [4, 2], [4, 3], [3, 2]]
+puts
+puts
+puts
+if user_world == correct_answer
+   puts "Test Pass"
+else
+    puts "Test fail"
+end
