5364eb96d092b38c70b3457beeb37316f0ac7ad7
Saurabh Patil <saurabhpatil97@hotmail.com>
2020-08-24 21:01:17 -0400

    (:bug:) : Add the test function

--- perl/gol_bug.pl
+++ perl/gol_bug.pl
@@ -1,13 +1,42 @@
 #!/usr/bin/perl
 
+=pod
+Here, X --> alive cell
+      O --> dead cell
+
+Input Pattern : 
+0 0 0 0 0 
+0 0 X 0 0
+0 0 X 0 0
+0 0 X 0 0 
+0 0 0 0 0
+Output Pattern :
+If the generation number is odd : | If the generation number is even :
+								  |
+0 0 0 0 0                         | 0 0 0 0 0
+0 0 0 0 0                         | 0 0 X 0 0
+0 X X X 0                         | 0 0 X 0 0
+0 0 0 0 0                         | 0 0 X 0 0
+0 0 0 0 0                         | 0 0 0 0 0
+								  |
+To print for debugging use :
+print <variable>;  ......To print scalar quantities like int,string
+print Dumper(\@arr);  .......To print data structure like arrays,hash
+=cut
+
 use Data::Dumper;
 use warnings;
+no warnings 'experimental::smartmatch';
 
-my @inp_arr;
 my $i = 0;
-
+# number of generations/iterations
 my $gen = 5;
 chomp($gen);
+# input array
+my @inp_arr = ([0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]);
+my @checker_arr = ([0,0,0,0,0],[0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0]);
+my @final_arr;
+
 
 ## helper
 sub getValue {
@@ -22,29 +51,36 @@ sub getValue {
         }
 }
 
-## main
-sub main {
-    open (FH,'<', "input.txt") or die "Cannot open file";
-    while (my $line = <FH>){
-    chomp($line);
-    $inp_arr[$i] = [split(",", $line)];
-    $i++;
+# test function
+sub tester {
+    my @list = @_;
+    $checksum = $list[0];
+    if ($checksum == 99) {
+        if (@final_arr ~~ @checker_arr){
+            print "Tests passed\n";
+        } else {
+            print "Wrong patterns generated\n"
+        }
+    } else {
+        print "\nFailed!!\n";
+    }
 }
-close(FH);
 
+## main function
+sub main {
+
+my $check = 0;
 @print_arr = map { [@$_] } @inp_arr;
 for (my $i = 0; $i < $gen; $i++){
-    print "Generation: $i\n";
-    my $e = 1;
-    while($e) {
-	print map((map($_ ? "X" : "o", @$_), "\n"), @inp_arr);
-	$e--;
-    }
-    
-    print "\n\n";
+    #print "Generation: $i\n";
+	#print map((map($_ ? "X " : "O ", @$_), "\n"), @inp_arr);
+    #print "\n\n";
     my $row = @inp_arr;
-    for my $j (1 .. $#inp_arr) {
-        for $k (1..$#{ $inp_arr[$i] }) { 
+    for my $j (0 .. $inp_arr) {
+        if ($j == 2){
+        $check = 99;
+        }
+        for $k (0..$#{ $inp_arr[$i] }) { 
             my $count = 0;
         for ( [$j-1, $k-1], [$j-1, $k], [$j-1, $k+1],[ $j, $k-1],[ $j, $k+1],[ $j+1, $k-1], [ $j+1, $k], [ $j+1, $k+1]) {
             my $aa = $_->[0];
@@ -54,6 +90,7 @@ for (my $i = 0; $i < $gen; $i++){
             }
 
         }
+        # checker to make cell dead/alive
         if ($inp_arr[$j][$k] == 0){
                 if ($count == 2 || $count == 3) {
                     $print_arr[$j][$k] = 1;
@@ -61,6 +98,7 @@ for (my $i = 0; $i < $gen; $i++){
                     $print_arr[$j][$k] = 0;
 		}
         }
+        # checker to make cell dead/alive
         if ($inp_arr[$j][$k] == 1) {
                 if ($count == 3) {
                     $print_arr[$j][$k] = 1;
@@ -73,6 +111,8 @@ for (my $i = 0; $i < $gen; $i++){
     }
 
     @inp_arr = map { [@$_] } @print_arr;
-}
+    }
+@final_arr = map { [@$_] } @inp_arr;
+tester($check)
 }
 main()
